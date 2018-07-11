58b5e443267265d66c72fb81411b9aca4210e8be
Abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-07-10 17:28:41 -0700

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,10 @@
+// Create variables and convert Celsius to Fahrenheit.
+var temperatureInCelsius = 40;
+var celsiusToFahrenheit = (temperatureInCelsius * 9)/5+32;
+console.log("Temperature of" + " " + temperatureInCelsius + " " + "celsius, will be" +
+ " " + celsiusToFahrenheit + " " + "fahrenheit");
+ // Create variables and convert Fahrenheit to Celsius.
+ var temperatureInFahrenheit = 104;
+ var fahrenheitToCelsius = ((temperatureInFahrenheit - 32)*5)/9
+ console.log("Temperature of" + " " + temperatureInFahrenheit + " " + "fahrenheit," + 
+ " " + "will be" + " " + fahrenheitToCelsius + " " + "celsius");
